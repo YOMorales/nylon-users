cd662dd97b870676dd85b9239b7128f20cd2fdeb
YOMorales <yomorales@gmail.com>
2024-03-20 22:56:13 -0500

    Did backend code for saving user data:
    *Also did class for validating data.

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,12 +2,28 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\UserCreateRequest;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Throwable;
 
 class UsersController extends Controller
 {
-    // home
-    // create
     // admin_list
     // activate/deactivate
+    /**
+     * @param UserCreateRequest $request
+     */
+    public function create(UserCreateRequest $request): JsonResponse
+    {
+        try {
+            $validated = $request->validated();
+
+            User::create($validated);
+
+            return response()->json('Success');
+        } catch (Throwable $th) {
+            return response()->json($th->getMessage());
+        }
+    }
 }
