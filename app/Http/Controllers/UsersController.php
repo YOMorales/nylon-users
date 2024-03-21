763e3d4b27602ae5a07f51588c7199d853f3485c
YOMorales <yomorales@gmail.com>
2024-03-20 23:26:25 -0500

    Did backend code for retrieving users list.

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,11 +5,11 @@ namespace App\Http\Controllers;
 use App\Http\Requests\UserCreateRequest;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
+use Illuminate\View\View;
 use Throwable;
 
 class UsersController extends Controller
 {
-    // admin_list
     // activate/deactivate
     /**
      * @param UserCreateRequest $request
@@ -25,5 +25,19 @@ class UsersController extends Controller
         } catch (Throwable $th) {
             return response()->json($th->getMessage());
         }
+    }
+
+    /**
+     * @return JsonResponse
+     */
+    // DEV NOTE: after building this action that serves JSON, I realized that it would be best suited for an API controller
+    public function adminGetUsers(): JsonResponse
+    {
+        // DEV NOTE: I'm defaulting to a low number of items per page because I expect the dummy data in the database to be small
+        $itemsPerPage = request('itemsPerPage', 2);
+
+        $users = User::select(['id', 'first_name', 'last_name', 'email', 'ssn_last_four'])->paginate($itemsPerPage);
+
+        return response()->json($users);
     }
 }
